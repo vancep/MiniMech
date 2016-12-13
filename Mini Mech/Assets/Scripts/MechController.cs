ccc2c843b7a5d420a00a96c3e1cad7050247c6f9
Vance <vancepiscitelli@gmail.com>
2016-12-13 01:23:34 -0800

    Can now control each pivot point with script. Rearranged structure of mech gameobject such that it makes more sense from a joint perspective.

--- Mini Mech/Assets/Scripts/MechController.cs
+++ Mini Mech/Assets/Scripts/MechController.cs
@@ -1,9 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
+
+enum pivot
+{
+	rightShoulderPivot, rightUpperArmPivot, rightLowerArmPivot, rightHandPivot, 
+	rightUpperLegPivot, rightLowerLegPivot, rightFootPivot, leftShoulderPivot, 
+	leftUpperArmPivot, leftLowerArmPivot, leftHandPivot, leftUpperLegPivot, 
+	leftLowerLegPivot, leftFootPivot, torsoPivot, headPivot
+}
 
 public class MechController : MonoBehaviour 
 {
+	public GameObject[] pivots = new GameObject[16];
+	public float[] rotateSpeed = new float[16];
+
+	private Quaternion[] targetRotation;
+	private bool[] rotatePivot;
+
+	/*
 	public GameObject rightShoulderPivot;
 	public GameObject rightUpperArmPivot;
 	public GameObject rightLowerArmPivot;
@@ -20,16 +34,68 @@ public class MechController : MonoBehaviour
 	public GameObject leftFootPivot;
 	public GameObject torsoPivot;
 	public GameObject headPivot;
+	*/
 
 	// Use this for initialization
 	void Start () 
 	{
-	
+		targetRotation = new Quaternion[pivots.Length];
+		rotatePivot = new bool[pivots.Length];
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-	
+		if(Input.GetKey(KeyCode.R))
+		{
+			RandomRotationTest();	
+		}
+
+		for(int i = 0; i < pivots.Length; i++)
+		{
+			if(rotatePivot[i])
+			{
+				UpdatePivot(i);
+
+				if(pivots[i].transform.rotation == targetRotation[i])
+				{
+					rotatePivot[i] = false;
+				}
+			}
+		}
+
+	}
+
+	/// <summary>
+	/// /*Randomly makes each pivot go to some rotation.
+	/// Which makes the mech look REALLY weird.*/
+	/// </summary>
+	private void RandomRotationTest()
+	{
+		for(int i = 0; i < 16; i++)
+		{
+			float x = Random.value * 360.0f;
+			float y = Random.value * 360.0f;
+			float z = Random.value * 360.0f;
+
+			RotatePivot(i, new Vector3(x, y, z));
+		}
+	}
+
+	private void RotatePivot(int p, Quaternion direction)
+	{
+		targetRotation[p] = direction;
+		rotatePivot[p] = true;
+	}
+
+	private void RotatePivot(int p, Vector3 direction)
+	{
+		targetRotation[p].eulerAngles = direction;
+		rotatePivot[p] = true;
+	}
+
+	private void UpdatePivot(int p)
+	{
+		pivots[p].transform.rotation = Quaternion.RotateTowards(pivots[p].transform.rotation, targetRotation[p], rotateSpeed[p] * Time.deltaTime);
 	}
 }
