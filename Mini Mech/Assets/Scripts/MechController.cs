a3ad9f9050fc7dcd2455c31307c4ad7ca87f94da
Vance <vancepiscitelli@gmail.com>
2016-12-12 23:23:13 -0800

    First commit. Current have a skeleton of a model and the start of a script.

---
+++ Mini Mech/Assets/Scripts/MechController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MechController : MonoBehaviour 
+{
+	public GameObject rightShoulderPivot;
+	public GameObject rightUpperArmPivot;
+	public GameObject rightLowerArmPivot;
+	public GameObject rightHandPivot;
+	public GameObject rightUpperLegPivot;
+	public GameObject rightLowerLegPivot;
+	public GameObject rightFootPivot;
+	public GameObject leftShoulderPivot;
+	public GameObject leftUpperArmPivot;
+	public GameObject leftLowerArmPivot;
+	public GameObject leftHandPivot;
+	public GameObject leftUpperLegPivot;
+	public GameObject leftLowerLegPivot;
+	public GameObject leftFootPivot;
+	public GameObject torsoPivot;
+	public GameObject headPivot;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
